18a0da2057e0e349405fd83e0c21970359724950
Peter <schmidt.multimedia@gmail.com>
2022-01-31 09:11:36 +0100

    01 default-Konstruktor / Referenzvariable

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,7 +3,14 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+    
+        // Cat --> Referenztyp / reference type
+        // cat --> Referentvariable / reference variable
+        // new Cat() --> Call des default-Konstruktors
+        Cat cat = new Cat();
+        System.out.println(cat);
+
+
     }
 
 }
