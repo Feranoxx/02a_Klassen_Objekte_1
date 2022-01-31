5ebd3afd2de12da608ba606f4e45b1df439cf9ef
Peter <schmidt.multimedia@gmail.com>
2022-01-31 12:29:32 +0100

    Custom-Konstruktor (Argumente / Parameter)

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,22 +4,14 @@ public class App {
     
     public static void main(String[] args) {
     
-        Cat cat1 = new Cat();
-        cat1.name = "Grizabella";
-        cat1.furColor = "white";
-        cat1.age = 29;
-        
+        Cat cat1 = new Cat("Grizabella", "white", 29);
         cat1.tellYourName();
         cat1.tellYourFurColor();
         cat1.tellYourAge();
 
         System.out.println("-------------");
 
-        Cat cat2 = new Cat();
-        cat2.name = "Alonzo";
-        cat2.furColor = "grey";
-        cat2.age = 35;
-        
+        Cat cat2 = new Cat("Alonzo", "grey", 35);
         cat2.tellYourName();
         cat2.tellYourFurColor();
         cat2.tellYourAge();
